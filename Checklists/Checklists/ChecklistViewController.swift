7f1bc3b9b0673c37b9fac3d8b17b1e4c38c24f4c
Faiq Ali <faiqueali017@gmail.com>
2020-11-15 20:50:14 +0500

    Added commit editing style of tableview

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -84,6 +84,15 @@ class ChecklistViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        items.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+        
+    }
+    
     //MARK:- Helper Methods
     func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
         

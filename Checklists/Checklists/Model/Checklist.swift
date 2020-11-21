b8da378a0827a03085113dcec67e9ab5c8548c67
Faiq Ali <faiqueali017@gmail.com>
2020-11-21 20:06:36 +0500

    replace countUncheckedItems() body with functional programming

--- Checklists/Checklists/Model/Checklist.swift
+++ Checklists/Checklists/Model/Checklist.swift
@@ -18,11 +18,21 @@ class Checklist: NSObject, Codable {
     }
     
     func countUncheckedItems() -> Int {
+        /*
         var count = 0
         for item in items where !item.checked {
             count += 1
         }
         return count
+ */
+        /**
+         reduce() is a method that looks at each item in the array and perfroms the code in the {} block. Initially, the cnt variable contains the value 0, but after each item it is incremented by either 0 or 1, depending on whether the item was checked-that check is done using our new friend, the ternary operator.
+         
+         When reduce() is done, its return value is the total count if unchecked items.
+         */
+        return items.reduce(0) {
+            cnt, item in cnt + (item.checked ? 0 : 1)
+        }
     }
     
 }

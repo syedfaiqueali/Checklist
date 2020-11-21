33ea20ddfbe4531eebc16141c14c72e0ead60eae
Faiq Ali <faiqueali017@gmail.com>
2020-11-21 18:13:25 +0500

    Bug fix for Fatal error: Index out of range

--- Checklists/Checklists/Controller/AllListsViewController.swift
+++ Checklists/Checklists/Controller/AllListsViewController.swift
@@ -32,7 +32,7 @@ class AllListsViewController: UITableViewController {
         navigationController?.delegate = self
         
         let index = dataModel.indexOfSelectedChecklist
-        if index != -1 {
+        if index >= 0 && index < dataModel.lists.count {
             let checklist = dataModel.lists[index]
             performSegue(withIdentifier: "ShowChecklist", sender: checklist)
         }

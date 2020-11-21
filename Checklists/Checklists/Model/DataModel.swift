373e742d94e1b7734c9e710dc55a31ac258a0616
Faiq Ali <faiqueali017@gmail.com>
2020-11-21 18:20:58 +0500

    handleFirstTime() added for the first time app launch

--- Checklists/Checklists/Model/DataModel.swift
+++ Checklists/Checklists/Model/DataModel.swift
@@ -23,6 +23,7 @@ class DataModel {
     init() {
         loadChecklist()
         registerDefaults()
+        handleFirstTime()
     }
     
     //MARK:- Data Saving
@@ -66,8 +67,21 @@ class DataModel {
     }
     
     func registerDefaults(){
-        let dictionary = ["ChecklistIndex": -1]
+        let dictionary = ["ChecklistIndex": -1, "FirstTime": true] as [String: Any]
         UserDefaults.standard.register(defaults: dictionary)
+    }
+    
+    func handleFirstTime(){
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime{
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+        }
     }
     
 }

fd015b3ec4aabfdd4a69174608f58821c3d84db2
Faiq Ali <faiqueali017@gmail.com>
2020-11-27 19:58:38 +0500

    i) Set AppDelegate for local notifications ii) Set varieties in ChecklistItem iii) nextChecklistItemID() added in DataModel

--- Checklists/Checklists/AppDelegate.swift
+++ Checklists/Checklists/AppDelegate.swift
@@ -6,15 +6,28 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Notification authorization
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
         return true
+    }
+    
+    //MARK:- User Notification Delegates
+    /**Will be invoked when the local notification is posted and the app is still running.
+       When our app is active and in the foreground, it is supposed to handle any fired notifications on its own.*/
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Received local notification \(notification)")
     }
 
     // MARK: UISceneSession Lifecycle
--- Checklists/Checklists/Model/ChecklistItem.swift
+++ Checklists/Checklists/Model/ChecklistItem.swift
@@ -10,4 +10,9 @@ import Foundation
 class ChecklistItem: NSObject, Codable {
     var text = ""
     var checked = false
+    
+    var dueDate = Date()
+    var shouldRemind = false
+    var itemID = -1  //id is a special keyword in ObjC thats why using itemID
+    
 }
